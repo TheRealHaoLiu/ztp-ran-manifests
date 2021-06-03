a28b46c6d14f9e7a5c7b50a2fe94cb071dcd59a4
Hao Liu <haoli@redhat.com>
2021-06-03 11:45:02 -0400

    add random vlan number generation

--- gen-moc-sites.sh
+++ gen-moc-sites.sh
@@ -7,8 +7,12 @@ PREFIX="sno"
 
 for i in $(seq -f "%05g" $START_INDEX $END_INDEX); do
   mkdir -p manifests/moc-sites/$PREFIX$i
-  for template_file in `ls moc-site-template`; do 
+  for template_file in `ls moc-site-template`; do
+    vlan=$(( $RANDOM % 4096 ))
     file_name=`echo $template_file | sed s/SITE/$PREFIX$i/`
-    cat moc-site-template/$template_file | sed s/SITE/$PREFIX$i/g > manifests/moc-sites/$PREFIX$i/$file_name
+    cat moc-site-template/$template_file | \
+        sed s/SITE/$PREFIX$i/g | \
+        sed s/VLAN/$vlan/g \
+        > manifests/moc-sites/$PREFIX$i/$file_name
   done
 done
