ae1637ce99cb6b18209f22befee22a3f53924341
testadmin <testadmin@redhat.com>
2021-07-14 09:53:51 -0400

    add split app
    Signed-off-by: testadmin <testadmin@redhat.com>

---
+++ argocd-applications/apply.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+kubeconfig=$KUBECONFIG
+GIT_HOST="https://gogs-svc-default.apps.izhang-hub-47-6np2g.dev10.red-chesterfield.com/testadmin/ztp-ran-manifests.git"
+SUB_CMD="apply"
+
+DEBUG="false"
+DEBUG_FILES=("gitops-cluster-rolebinding.yaml" "app-project.yaml" "common-app.yaml")
+
+while getopts "k:c:shd?" opt; do
+    case "${opt}" in
+    k)
+        kubeconfig="${OPTARG}"
+        ;;
+    s)
+        GIT_HOST="https://gogs-svc-default.apps.bm.rdu2.scalelab.redhat.com/testadmin/ztp-ran-manifests.git"
+        ;;
+    c)
+        SUB_CMD="${OPTARG}"
+        ;;
+    d)
+        DEBUG="true"
+        echo
+        echo "Running in debug mode, only ${DEBUG_FILES[@]} will be processed"
+        ;;
+    h | ? | *)
+        echo
+        echo "$(basename $0) will apply or delete argocd application by override the"
+        echo "*yaml files in this dircrtory"
+        echo
+        echo "If runs in debug mode, then only ${DEBUG_FILES[@]} will be processed"
+        echo
+        echo "-k <kubeconfig path>, to specify your kubeconfig, default is KUBECONFIG"
+        echo "-c <sub-command nmae>, to specify kubectl's subcommand, default is apply"
+        echo "-d, flag will decide run in debug mode or not"
+        echo "-s, flag let the script knows your are running againt scale-lab env"
+        exit 0
+        ;;
+    esac
+done
+
+KUBECTL_CMD="oc --kubeconfig $kubeconfig --insecure-skip-tls-verify=true"
+
+echo
+echo "$(basename $0) runs at context: "
+echo "$($KUBECTL_CMD config current-context)"
+echo
+
+if [ "$DEBUG" == "true" ]; then
+    for file in ${DEBUG_FILES[@]}; do
+        cat $file | sed "s|GIT_HOST|${GIT_HOST}|" | $KUBECTL_CMD $SUB_CMD -f -
+    done
+
+    exit $#
+fi
+
+for file in $(ls); do
+    cat $file | sed "s|GIT_HOST|${GIT_HOST}|" | $KUBECTL_CMD $SUB_CMD -f -
+done
--- gen-moc-sites.sh
+++ gen-moc-sites.sh
@@ -5,15 +5,32 @@ END_INDEX=$2
 
 PREFIX="sno"
 
+#manifests/moc-sites/app1/prefix$1/$file_name
+#
+#app: manifests/moc-sites/app1{prefix 1:100}
+
+APP_PREFIX="app"
+APP_COUNTER="1"
+
 rm -rf manifests/moc-sites
-for i in $(seq -f "%05g" $START_INDEX $END_INDEX); do
-  mkdir -p manifests/moc-sites/$PREFIX$i
-  for template_file in `ls moc-site-template`; do
-    vlan=$(( $RANDOM % 4096 ))
-    file_name=`echo $template_file | sed s/SITE/$PREFIX$i/`
-    cat moc-site-template/$template_file | \
-        sed s/SITE/$PREFIX$i/g | \
-        sed s/VLAN/$vlan/g \
-        > manifests/moc-sites/$PREFIX$i/$file_name
-  done
+for i in $(seq $START_INDEX $END_INDEX); do
+    if [ $((i % 100)) -eq 0 ]; then
+        ((APP_COUNTER += 1))
+    fi
+
+    idx=$(printf "%05g" $i)
+    appIdx=$(printf "%03g" $APP_COUNTER)
+
+    appPath="manifests/moc-sites/$APP_PREFIX-$appIdx/$PREFIX$idx"
+    mkdir -p $appPath
+
+    for template_file in $(ls moc-site-template); do
+        vlan=$(($RANDOM % 4096))
+        file_name=$(echo $template_file | sed s/SITE/$PREFIX$i/)
+        cat moc-site-template/$template_file |
+            sed s/SITE/$PREFIX$i/g |
+            sed s/VLAN/$vlan/g \
+                > $appPath/$file_name
+    done
+
 done
